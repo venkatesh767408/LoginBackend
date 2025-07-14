c8d084b0c42d0ecf4f81583edac135f36927923a
CHAMRTHI VENKATESH <160413645+venkatesh767408@users.noreply.github.com>
2025-07-14 12:08:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
 mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(process.env.PORT, () => console.log("Server running on port", process.env.PORT)))
   .catch(err => console.error("MongoDB error:", err));
